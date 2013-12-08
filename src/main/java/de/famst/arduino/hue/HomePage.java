2cf0e8d1e66d79e12f3cb9fa82f833e036390dc4
Jens Stockhausen <jens@famst.de>
2013-12-08 16:34:22 +0100

    first draft of gui

--- src/main/java/de/famst/arduino/hue/HomePage.java
+++ src/main/java/de/famst/arduino/hue/HomePage.java
@@ -1,16 +1,110 @@
-
 package de.famst.arduino.hue;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.validator.RangeValidator;
+
+import de.famst.arduino.hue.com.ArduinoTCPServer;
 
 public class HomePage extends WebPage
 {
+  @SpringBean
+  private ArduinoTCPServer arduinoTCPServer;
+  
+  
   private static final long serialVersionUID = 1L;
+
+  private class RGBInputForm extends Form
+  {
+    private Integer valueR;
+    private Integer valueG;
+    private Integer valueB;
+    
+    public Integer getValueR()
+    {
+      return valueR;
+    }
+
+    public void setValueR(Integer valueR)
+    {
+      this.valueR = valueR;
+    }
+
+    public Integer getValueG()
+    {
+      return valueG;
+    }
+
+    public void setValueG(Integer valueG)
+    {
+      this.valueG = valueG;
+    }
+
+    public Integer getValueB()
+    {
+      return valueB;
+    }
+
+    public void setValueB(Integer valueB)
+    {
+      this.valueB = valueB;
+    }
+    
+    private static final long serialVersionUID = 1L;
+
+    public RGBInputForm(String id)
+    {
+      super(id);
+      
+      valueR = 0;
+      valueG = 0;
+      valueB = 0;
+      
+      setDefaultModel(new CompoundPropertyModel(this));
+      
+      add(new TextField<Integer>("valueR", Integer.class).setRequired(true)
+          .add(new RangeValidator<Integer>(0, 255)));
+
+      add(new TextField<Integer>("valueG", Integer.class).setRequired(true)
+          .add(new RangeValidator<Integer>(0, 255)));
+
+      add(new TextField<Integer>("valueB", Integer.class).setRequired(true)
+          .add(new RangeValidator<Integer>(0, 255)));
+
+      add(new Button("applyButton"));
+      
+//      {
+//        private static final long serialVersionUID = 1L;
+//
+//        @Override
+//        public void onSubmit()
+//        {
+//          @SuppressWarnings("unused")
+//          Integer red = getValueR();
+//        }
+//      }.setDefaultFormProcessing(false));
+
+    }
+
+    @Override
+    protected void onSubmit()
+    { 
+      arduinoTCPServer.setColor(getValueR(), getValueG(), getValueB());
+    }
+
+  }
 
   // called by WicketApplication
   public HomePage()
   {
     super();
+
+    add(new RGBInputForm("inputForm"));
+
   }
 
 }
--- src/main/java/de/famst/arduino/hue/com/ArduinoTCPServer.java
+++ src/main/java/de/famst/arduino/hue/com/ArduinoTCPServer.java
@@ -3,8 +3,19 @@ package de.famst.arduino.hue.com;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import javax.annotation.ManagedBean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.esotericsoftware.minlog.Log;
+
+@ManagedBean
 public class ArduinoTCPServer
 {
+  private static final Logger LOG = LoggerFactory
+      .getLogger(ArduinoTCPServer.class);
+
   private BlockingQueue<String> queue;
   private SocketThread thread = null;
 
@@ -15,35 +26,12 @@ public class ArduinoTCPServer
 
     try
     {
+      // blick when connected
       queue.put("#SET|000000000\n");
       queue.put("#SET|255000000\n");
       queue.put("#SET|000255000\n");
       queue.put("#SET|000000255\n");
       queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-      queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-      queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-      queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-      queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-      queue.put("#SET|000000000\n");
-      queue.put("#SET|255000000\n");
-      queue.put("#SET|000255000\n");
-      queue.put("#SET|000000255\n");
-
     }
     catch (InterruptedException e)
     {
@@ -56,12 +44,21 @@ public class ArduinoTCPServer
   {
     try
     {
+      LOG.info("Enqueing:" + message);
       queue.put(message);
     }
     catch (InterruptedException e)
     {
       e.printStackTrace();
     }
+  }
+
+  public void setColor(Integer valueR, Integer valueG, Integer valueB)
+  {
+    String message = String.format("#SET|%03d%03d%03d\n", valueR, valueG,
+        valueB);
+
+    sendMessage(message);
   }
 
 }
